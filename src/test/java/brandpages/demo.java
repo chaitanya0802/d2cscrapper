eb581985874e1449132d9f7dc85cdc217601edb8
Chaitanya Malshikare <chaitanya7447@gmail.com>
2025-10-10 17:37:45 +0530

    changes

--- src/test/java/brandpages/demo.java
+++ src/test/java/brandpages/demo.java
@@ -10,38 +10,65 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.ConfigReader;
+import utils.Product;
 
 public class demo {
-    
+
     String storename = "Boat";
     String maincat = "Electronics";
 
     String categoryUrl = "https://www.boat-lifestyle.com/collections/true-wireless-earbuds";
     String productContainerElement = "//*[@id='facet-main']/product-list";
     String productItemElement = "//*[@id='Huratips_Loop']//product-item";
-    String productNameElement = "";
 
+    String productNameElement = "";
+    String productURLElement = "";
+    String productImageURLElement = "";
+    String productPriceElement = "";
+    String productDesElement = "";
+    String productratingElement = "";
+    String productAvailElement = "";
+
+    // catname , catlink
+    Map<String, String> subcat = Map.of(
+            "Party Speakers" , "https://www.boat-lifestyle.com/collections/party-speakers"
+            // "Bluetooth Neckbands Earphones" , "http://boat-lifestyle.com/collections/bluetooth-neckbands-earphones",
+            // "Wireless Earbuds", "https://www.boat-lifestyle.com/collections/true-wireless-earbuds",
+            // "Wireless Speakers", "https://www.boat-lifestyle.com/collections/wireless-speakers"
+    );
+
+    WebDriver driver;
 
     @Test
     public void scrapController() {
-        if (Boolean.parseBoolean(ConfigReader.getProperty("product"))) scrapProdData();
+        if (Boolean.parseBoolean(ConfigReader.getProperty("product")))
+            scrapProdData();
 
-        // if (Boolean.parseBoolean(ConfigReader.getProperty("offer"))) scrapOfferData();
+        // if (Boolean.parseBoolean(ConfigReader.getProperty("offer")))
+        // scrapOfferData();
 
     }
 
-
     public void scrapProdData() {
-
-        int prevProductCount = 0, sameCountTries = 0, scrolls = 0, maxScrolls = 100;
 
         // driver config
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get(categoryUrl);
+        driver = new ChromeDriver();
 
-        //scroll at the end
-        while(scrolls < maxScrolls) {
+        for (Map.Entry<String, String> m : subcat.entrySet()) {
+            System.out.println("==> " + m.getKey() + " ==>" + m.getValue());
+            scrapCategoryData(m.getKey(), m.getValue());
+        }
+    }
+
+    public void scrapCategoryData(String subcat, String catURL) {
+        List<Product> productList = new ArrayList<>();
+        int prevProductCount = 0, sameCountTries = 0, scrolls = 0, maxScrolls = 100;
+
+        driver.navigate().to(catURL);
+
+        // scroll at the end
+        while (scrolls < maxScrolls) {
 
             // scroll to bottom of product container
             JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -53,8 +80,8 @@ public class demo {
                 System.out.println("Product container Element not found in DOM");
                 break;
             }
-           
-            //sleep
+
+            // sleep
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
@@ -63,64 +90,68 @@ public class demo {
 
             // track total products loaded in current scroll
             int currProducts;
-            try{
+            try {
                 currProducts = driver.findElements(By.xpath(productItemElement)).size();
             } catch (NoSuchElementException e) {
                 System.out.println("ProductItemElement not found in DOM");
                 break;
             }
 
-            // scroll by 10 for lazy loading check if product container cannot be loaded more
+            // scroll by 10 for lazy loading check if product container cannot be loaded
+            // more
             if (currProducts == prevProductCount) {
                 js.executeScript("window.scrollBy(0, 10);");
                 sameCountTries++;
-                if (sameCountTries >= 5) break;
+                if (sameCountTries >= 5)
+                    break;
             } else {
                 sameCountTries = 0;
             }
-            //update
+            // update
             prevProductCount = currProducts;
             scrolls++;
 
         }
 
-        System.out.println("Total ProductS Found: " + prevProductCount);
-
-        //if total products are 0 then is failed scrapping
+        // if total products are 0 then is failed scrapping
         Assert.assertNotEquals(prevProductCount, 0);
 
-
-        //extract the product data
+        // extract the product data
         List<WebElement> products = driver.findElements(By.xpath(productItemElement));
 
-        for(int i=0; i<products.size(); i++){
-            try{
+        System.out.println("##=== Total products for("+ subcat + ")category are: " + products.size());
+
+        for (int i = 0; i < products.size(); i++) {
+            try {
 
                 WebElement link = products.get(i).findElement(By.xpath(".//div/div[2]/div/div/a"));
 
-                    String name = link.getText().trim();
-                    String url = link.getAttribute("href");
-                    String id = storename + Math.abs(url.hashCode());
-                    String imageurl = products.get(i).findElement(By.xpath(".//div/div[1]/div/a/img")).getAttribute("src");
-                    String price = products.get(i).findElement(By.xpath(".//div/div[2]/div/div/div[1]/div[1]/span[1]")).getAttribute("data-price").trim();
-                    int prodprice = Integer.parseInt(price.replaceAll("[^0-9]", "")) / 100;
-                    String des = products.get(i).findElement(By.xpath(".//div/div[2]/div/div/div[1]/div[2]")).getText().trim().replaceAll("[™®]", "").trim();
-                    String rt = products.get(i).findElement(By.xpath(".//div/div[2]/div/span/div/div")).getText().trim();
-                    Float rating = (Float) Float.parseFloat(rt);
+                String name = link.getText().trim();
+                String url = link.getAttribute("href");
+                String id = storename + Math.abs(url.hashCode());
+                String imageurl = products.get(i).findElement(By.xpath(".//div/div[1]/div/a/img")).getAttribute("src");
+                String price = products.get(i).findElement(By.xpath(".//div/div[2]/div/div/div[1]/div[1]/span[1]"))
+                        .getAttribute("data-price").trim();
+                int prodprice = Integer.parseInt(price.replaceAll("[^0-9]", "")) / 100;
+                String des = products.get(i).findElement(By.xpath(".//div/div[2]/div/div/div[1]/div[2]")).getText()
+                        .trim().replaceAll("[™®]", "").trim();
+                String rt = products.get(i).findElement(By.xpath(".//div/div[2]/div/span/div/div")).getText().trim();
+                Float rating = (Float) Float.parseFloat(rt);
+                                
+                boolean isAvailable = !products.get(i).findElements(By.xpath(
+                    ".//descendant::*[contains(text(), 'Add to cart')]"
+                )).isEmpty();
 
-                    WebElement container = products.get(i).findElement(By.xpath(".//div/div[2]/div/div/div[2]"));
-                    String tag = container.findElement(By.xpath("./*")).getTagName();
-                    boolean isAvailable = tag.equals("form");  // true if form → available
+                Product p = new Product(id, name, url, imageurl, maincat, subcat, prodprice, des, rating, isAvailable);
+                productList.add(p);
 
-                    Product p = new Product(id, name, url, imageurl, maincat, subcat, prodprice, des, rating, isAvailable);
-                    productList.add(p);
-
-                    System.out.println(p);
-                    System.out.println("===============");
+                System.out.println(p);
+                System.out.println("==========================");
 
             } catch (NoSuchElementException e) {
-                System.out.println("Product Element not found in DOM");
-                break;
+                System.out.println("Product structure mismatch");
+            } catch (Exception e) {
+                System.out.println(e.toString());
             }
         }
 
